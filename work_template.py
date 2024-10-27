5378178fca7395ae413d4aae0bca6397c18ed1fe
nicolaslieb <nicolas.liebau@student.fh-kiel.de>
2024-10-27 14:40:19 +0000

    Added the work template from the professor

---
+++ work_template.py
@@ -0,0 +1,62 @@
+# Importing the required libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import skfuzzy as fuzz  # Uncomment if you're using fuzzy logic for the application
+from pomegranate import *  # Uncomment if your project involves Bayesian Networks
+
+# Introduction
+print("\n\n# Técnicas Matemáticas para Big Data - Project NN?\n\n")
+
+# Group Information
+print("GROUP NN:")
+print("- Student 1 - Nº xxxxx - ??% Work Participation")
+print("- Student 2 - Nº xxxxx - ??% Work Participation")
+print("- Student 3 - Nº xxxxx - ??% Work Participation")
+
+# 1. Introduction to the problem of study
+print("\n\n## 1. Introduction to the problem of study [1,0 valor]")
+
+# 2. Brief and general description of the approach and methods used
+print("\n\n## 2. Brief and general description of the approach and methods used [1,5 valor]")
+
+# 3. Brief History and literature review of the problem and methods/algorithms
+print("\n\n## 3. Brief History and literature review of the problem and methods/algorithms [1,5 valor]")
+
+# 4. About the main method/algorithm used
+print("\n\n## 4. About the main method/algorithm used [1,5 valor]")
+
+# 5. Python imports and global configurations
+print("\n\n## 5. Python imports and global configurations [0,5 valor]")
+print("### Install and import the necessary libraries to compute the Bayesian Network and perform other methods")
+# Uncomment the following lines to install necessary packages if not already installed
+# %pip install pandas
+# %pip install seaborn
+# %pip install matplotlib
+# %pip install numpy
+# %pip install pomegranate
+# %pip install skfuzzy  # Uncomment if using fuzzy logic
+# %pip install torch
+# %pip install Pillow
+
+# 6. Dataset and variables explanation
+print("\n\n## 6. Dataset and variables explanation [1,5 valor]")
+
+# 7. Main code as possible solution to the problem
+print("\n\n## 7. Main code as possible solution to the problem [1,5 valor]")
+
+# 8. Analysis of Example 1
+print("\n\n## 8. Analysis of Example 1 [3,0 valor]")
+
+# 9. Analysis of Example 2
+print("\n\n## 9. Analysis of Example 2 [3,0 valor]")
+
+# 10. Pros and cons of the approach
+print("\n\n## 10. Pros and cons of the approach [2,0 valor]")
+
+# 11. Future improvements
+print("\n\n## 11. Future improvements [2,0 valor]")
+
+# References
+print("\n\n<div style=\"text-align: center;\">\n    <p style=\"font-size: 40px;\">References [1,0 valor]</p>\n</div>\n\n")
